a0b795e98c77642b66f5fd9493ef8d496678c58a
farismismar <faris_mismar@hotmail.com>
2022-02-24 15:07:20 -0600

    Add files via upload

--- find_sum.cpp
+++ find_sum.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 template<class T>
 ostream &operator<<(ostream &os, vector<T> v)
 {
-	os << '[';
+	os << '{';
 	if (v.size() > 0) {
 		for (unsigned int i = 0; i < v.size() - 1; i++)
 			os << v[i] << ", ";
 		os << v[v.size() - 1];
 	}
-	os << ']';
+	os << '}';
 
 	return os;
 }
 
-vector<int> return_elements(vector<int> &v, int sum)
+vector<int> return_elements_2(vector<int> &v, int sum)
+{
+	int target_value;
+	vector<int>::iterator index;
+
+	for (unsigned int i = 0; i < v.size(); i++) {
+		target_value = sum - v[i];
+		
+		index = find(v.begin(), v.end(), target_value);
+
+		if (index != v.end())
+			return vector<int>({v[i], target_value}); 
+	}
+	
+	cout << "A sum of two elements cannot be found." << endl;
+
+	return vector<int>();
+}
+
+vector<int> return_elements_1(vector<int> &v, int sum)
 {
 	int current_sum;
 
@@ -39,7 +59,7 @@ int main()
 	int sum = 7;
 
 	vector<int> result;
-	result = return_elements_1(w, sum);
+	result = return_elements_2(w, sum);
 
 	cout << result << endl;
 
